51d9cc374155881932979e14d12ab26a9dc5d36d
Lizl <237743663@qq.com>
2024-04-09 15:22:06 +0800

    集成ant-design-vue组件及其图标

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入ant-design-vue组件和css样式
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+// 引入ant-design-vue图标
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+// createApp(App).use(Antd).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(Antd).use(store).use(router).mount('#app')
+
+// 全局使用icons
+const icons = Icons
+for (const i in icons) {
+    app.component(i, icons[i])
+}
