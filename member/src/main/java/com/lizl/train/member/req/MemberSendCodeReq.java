cda8df4df0858ded85575fcebb11db6964396354
Lizl <237743663@qq.com>
2024-06-13 11:02:33 +0800

    增加短信验证码接口

---
+++ member/src/main/java/com/lizl/train/member/req/MemberSendCodeReq.java
@@ -0,0 +1,33 @@
+package com.lizl.train.member.req;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+/**
+ * @Author Lizl
+ * @Description
+ * @Date 2024/4/8 下午2:18
+ * @Version 1.0.0
+ */
+public class MemberSendCondeReq {
+
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1\\d{10}$", message = "手机号码格式错误")
+    private String mobile;
+
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberSendCondeReq{" +
+                "mobile='" + mobile + '\'' +
+                '}';
+    }
+}
